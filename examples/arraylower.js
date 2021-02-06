8a2603abe8f6d8bbc68e321362736ee9cbe62447
Thiccy <74794033+phaticusthiccy@users.noreply.github.com>
2021-02-06 20:41:58 +0300

    Create arraylower.js

---
+++ examples/arraylower.js
@@ -0,0 +1,8 @@
+const ARRAY_GEN = (x,y) => (function*(){
+  while (x <= y) yield x++;
+})();
+
+for (let res of ARRAY_GEN(1,5)){ 
+  // Push numbers from 1 to 5. (1 and 5 included)
+  console.log(res);
+}
